d64222059667f1f4399b50d3202f12e2d2f20d5d
carveyh <carvey.hor@gmail.com>
2023-03-31 09:06:06 -0400

    Completed Dessert spec

--- W4D5HW/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
+++ W4D5HW/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
@@ -41,16 +41,35 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      expect{ dessert.eat(3) }.to_not raise_error
+      expect(dessert.quantity).to eq(2)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect{dessert.eat(6)}.to raise_error "not enough left!"
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    # let(:chef) { double("chef", :name=>"Kaz") }
+    let(:chef) { double("chef") }
+    subject { Dessert.new("cookie", 10, chef) }
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Kaz the Great Baker")
+      expect(subject.serve).to eq("Chef Kaz the Great Baker has made 10 cookies!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert).and_return(400)
+      # expect(chef).to receive(:put_in_oven).with(dessert)
+      # expect(dessert).to receive(:heat!) 
+        #Is there a way to test that dessert.heat! gets called without
+        #defining the Chef class that calls it from within a helper method?
+      dessert.make_more
+      # expect(dessert.temp).to eq(400)
+    end
   end
 end
